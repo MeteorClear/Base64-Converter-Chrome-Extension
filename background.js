7beb1a6367a1f7e394cd16d0514d800744b623ce
MeteorClear <103306779+MeteorClear@users.noreply.github.com>
2024-02-21 16:03:25 +0900

    Add files via upload

--- background.js
+++ background.js
@@ -2,14 +2,15 @@ console.log("background.js load");
 
 let draggedText = '';
 
+
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     if (message.text) {
         draggedText = message.text;
     }
-});
 
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     if (message.action === 'getDraggedText') {
         sendResponse({ text: draggedText });
     }
+
+    console.log(draggedText);
 });
