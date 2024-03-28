a1dd7f440721395dd0660eaee1c10d7f5394c805
MeteorClear <103306779+MeteorClear@users.noreply.github.com>
2024-03-28 16:56:36 +0900

    Source code
    Add comment

--- background.js
+++ background.js
@@ -10,9 +10,9 @@ chrome.runtime.onMessage.addListener(
      * Save the text if the response contains text. 
      * 
      * When the 'getSelectedText' action from 'popup.js' comes in, the saved text is returned via 'sendResponse' callback.
-     * @param {object} message The message object received from 'content.js'
+     * @param {object} message The message object received from 'content.js'.
      * @param {object} sender The object that provides information about the context from which the message was sent, including the tab and frame id.
-     * @param {function} sendResponse The function that can be called to send a response back to the sender of the message
+     * @param {function} sendResponse The function that can be called to send a response back to the sender of the message.
      */
     function(message, sender, sendResponse) {
         if (message.text) {
@@ -28,37 +28,49 @@ chrome.runtime.onMessage.addListener(
 );
 
 
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.contextMenus.create({
-        'id': 'EncodeBase64',
-        'title': 'Encode Base64',
-        'contexts': ['selection']
-    });
+chrome.runtime.onInstalled.addListener(
+    /**
+     * Create custom items to the context menu that appears when you right-click on Chrome.
+     */
+    function() {
+        chrome.contextMenus.create({
+            'id': 'EncodeBase64',
+            'title': 'Encode Base64',
+            'contexts': ['selection']
+        });
 
-    chrome.contextMenus.create({
-        'id': 'DecodeBase64',
-        'title': 'Decode Base64',
-        'contexts': ['selection']
-    });
-});
-
-
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
-    if (info.menuItemId === 'EncodeBase64' && info.selectionText) {
-        console.log('context menu click:', tab.id, info.menuItemId);
-
-        chrome.tabs.sendMessage(tab.id, {
-            action: 'encodeBase64', 
-            text: info.selectionText
+        chrome.contextMenus.create({
+            'id': 'DecodeBase64',
+            'title': 'Decode Base64',
+            'contexts': ['selection']
         });
     }
+);
 
-    if (info.menuItemId === 'DecodeBase64' && info.selectionText) {
-        console.log('context menu click:', tab.id, info.menuItemId);
 
-        chrome.tabs.sendMessage(tab.id, {
-            action: 'decodeBase64', 
-            text: info.selectionText
-        });
+chrome.contextMenus.onClicked.addListener(
+    /**
+     * Listens for clicks on context menu items and sends messages to 'content.js'
+     * @param {object} info Information about the context menu click event.
+     * @param {object} tab Information about the active tab.
+     */
+    function(info, tab) {
+        if (info.menuItemId === 'EncodeBase64' && info.selectionText) {
+            console.log('context menu click:', tab.id, info.menuItemId);
+
+            chrome.tabs.sendMessage(tab.id, {
+                action: 'encodeBase64', 
+                text: info.selectionText
+            });
+        }
+
+        if (info.menuItemId === 'DecodeBase64' && info.selectionText) {
+            console.log('context menu click:', tab.id, info.menuItemId);
+
+            chrome.tabs.sendMessage(tab.id, {
+                action: 'decodeBase64', 
+                text: info.selectionText
+            });
+        }
     }
-});
+);
